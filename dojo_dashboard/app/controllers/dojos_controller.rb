630adb70a04ef3805cc6d1bdbe8a51aef7a259f9
Jacob Sexton <jshuman@protonmail.com>
2021-08-11 23:31:26 +0000

    90% functionality working. Still need errors for validation. Need styling.

--- dojo_dashboard/app/controllers/dojos_controller.rb
+++ dojo_dashboard/app/controllers/dojos_controller.rb
@@ -5,4 +5,13 @@ class DojosController < ApplicationController
         @count = Dojo.all.count
     end
 
+    def new
+    end
+    
+    def create
+    Dojo.create(branch: params[:dojo][:branch], street: params[:dojo][:street], city: params[:dojo][:city], state: params[:dojo][:state] )
+    flash[:success] = "You did it"
+    redirect_to '/dojos'
+    end
+
 end
